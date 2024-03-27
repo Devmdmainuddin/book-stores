b159ba44b8f652d0c631ebd94deb6dd0dbf5484c
devmdmainuddin <devmdmainuddin@gmail.com>
2024-03-27 23:18:13 +0600

    book read function fixed

--- src/utility/localstorage.js
+++ src/utility/localstorage.js
@@ -11,11 +11,35 @@ const getStoreData = () =>{
 const saveStoreData = id =>{
     const storedDatas = getStoreData();
     const exists = storedDatas.find(bookId => bookId === id);
-    if(!exists){
-        toast.success('books ar added wishlist Book')
-        storedDatas.push(id)
-        localStorage.setItem('storebooks', JSON.stringify(storedDatas))  
-    }
+    if(exists){
+        toast.error('book alredey in  wishlist !')
+    }  storedDatas.push(id)
+    localStorage.setItem('storebooks', JSON.stringify(storedDatas))  
 
 }
-export  { getStoreData, saveStoreData}
+
+
+
+
+const getreadbooksData = () =>{
+    const readData = localStorage.getItem('readbooks');
+    if(readData){
+        return JSON.parse(readData)
+    }  
+    return [];
+}
+
+const savereadbooksData = id =>{
+    const readbooksDatas = getreadbooksData();
+    const exists = readbooksDatas.find(bookId => bookId === id);
+    if (exists) {
+      return  toast.error('book alredey in  read book !')
+    }
+    readbooksDatas.push(id)
+    localStorage.setItem('readbooks', JSON.stringify(readbooksDatas))
+    toast.success('books ar added read Book') 
+
+}
+
+
+export  { getStoreData, saveStoreData,savereadbooksData,getreadbooksData}
